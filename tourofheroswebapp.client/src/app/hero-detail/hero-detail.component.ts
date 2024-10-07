7d1de16ee3b1c8139c2d9cf90871170343c291f0
Kyle <Kyle.Altemara@gmail.com>
2024-10-07 14:10:33 -0700

    Finished tutorial, get data from a server
    https://v17.angular.io/tutorial/tour-of-heroes/toh-pt6

--- tourofheroswebapp.client/src/app/hero-detail/hero-detail.component.ts
+++ tourofheroswebapp.client/src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,4 @@
-import { Component, Input } from '@angular/core';
-import { NgIf, UpperCasePipe } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
@@ -12,8 +10,9 @@ import { HeroService } from '../hero.service';
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css']
 })
-export class HeroDetailComponent {
-  @Input() hero?: Hero;
+export class HeroDetailComponent implements OnInit {
+  hero: Hero | undefined;
+
   constructor(
     private route: ActivatedRoute,
     private heroService: HeroService,
@@ -25,12 +24,19 @@ export class HeroDetailComponent {
   }
 
   getHero(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
+    const id = parseInt(this.route.snapshot.paramMap.get('id')!, 10);
     this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
   }
 
   goBack(): void {
     this.location.back();
+  }
+
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero)
+        .subscribe(() => this.goBack());
+    }
   }
 }
